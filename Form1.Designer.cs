2b1575098bb94ee72724e8d9f436c00064baea0d
Adham Desouky <126396682+AdhamDesouky@users.noreply.github.com>
2024-04-20 00:59:42 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,214 @@
+﻿namespace ConsoleApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt_Search_By_Name = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn_searchByName = new System.Windows.Forms.Button();
+            this.FindAllPatients_Btn = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_identity = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txt_fname = new System.Windows.Forms.TextBox();
+            this.txt_LastName = new System.Windows.Forms.TextBox();
+            this.txt_Email = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txt_Search_By_Name
+            // 
+            this.txt_Search_By_Name.Location = new System.Drawing.Point(174, 55);
+            this.txt_Search_By_Name.Name = "txt_Search_By_Name";
+            this.txt_Search_By_Name.Size = new System.Drawing.Size(215, 20);
+            this.txt_Search_By_Name.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(94, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Patient_Name";
+            // 
+            // btn_searchByName
+            // 
+            this.btn_searchByName.Location = new System.Drawing.Point(408, 53);
+            this.btn_searchByName.Name = "btn_searchByName";
+            this.btn_searchByName.Size = new System.Drawing.Size(75, 23);
+            this.btn_searchByName.TabIndex = 2;
+            this.btn_searchByName.Text = "Search";
+            this.btn_searchByName.UseVisualStyleBackColor = true;
+            this.btn_searchByName.Click += new System.EventHandler(this.Find_Patient_Btn_Click);
+            // 
+            // FindAllPatients_Btn
+            // 
+            this.FindAllPatients_Btn.Location = new System.Drawing.Point(96, 119);
+            this.FindAllPatients_Btn.Name = "FindAllPatients_Btn";
+            this.FindAllPatients_Btn.Size = new System.Drawing.Size(587, 23);
+            this.FindAllPatients_Btn.TabIndex = 3;
+            this.FindAllPatients_Btn.Text = "List_Patients";
+            this.FindAllPatients_Btn.UseVisualStyleBackColor = true;
+            this.FindAllPatients_Btn.Click += new System.EventHandler(this.Find_All_Patients_Btn_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(96, 148);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(587, 220);
+            this.dataGridView1.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(94, 392);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "FirstName";
+            // 
+            // txt_identity
+            // 
+            this.txt_identity.AutoSize = true;
+            this.txt_identity.Location = new System.Drawing.Point(94, 464);
+            this.txt_identity.Name = "txt_identity";
+            this.txt_identity.Size = new System.Drawing.Size(31, 13);
+            this.txt_identity.TabIndex = 6;
+            this.txt_identity.Text = "Email";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(94, 428);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(54, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "LastName";
+            // 
+            // txt_fname
+            // 
+            this.txt_fname.Location = new System.Drawing.Point(205, 389);
+            this.txt_fname.Name = "txt_fname";
+            this.txt_fname.Size = new System.Drawing.Size(139, 20);
+            this.txt_fname.TabIndex = 8;
+            // 
+            // txt_LastName
+            // 
+            this.txt_LastName.Location = new System.Drawing.Point(205, 421);
+            this.txt_LastName.Name = "txt_LastName";
+            this.txt_LastName.Size = new System.Drawing.Size(139, 20);
+            this.txt_LastName.TabIndex = 9;
+            // 
+            // txt_Email
+            // 
+            this.txt_Email.Location = new System.Drawing.Point(205, 461);
+            this.txt_Email.Name = "txt_Email";
+            this.txt_Email.Size = new System.Drawing.Size(139, 20);
+            this.txt_Email.TabIndex = 10;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(369, 389);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "Add_Patient";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Add_Patient_Btn_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(489, 53);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(93, 23);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Update";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Update_Patient_Btn_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(588, 53);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(95, 23);
+            this.button2.TabIndex = 14;
+            this.button2.Text = "Delete";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Delete_Patient_Btn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(824, 500);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.txt_Email);
+            this.Controls.Add(this.txt_LastName);
+            this.Controls.Add(this.txt_fname);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txt_identity);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.FindAllPatients_Btn);
+            this.Controls.Add(this.btn_searchByName);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txt_Search_By_Name);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_Search_By_Name;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btn_searchByName;
+        private System.Windows.Forms.Button FindAllPatients_Btn;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label txt_identity;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txt_fname;
+        private System.Windows.Forms.TextBox txt_LastName;
+        private System.Windows.Forms.TextBox txt_Email;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Hl7.Fhir.Model;
+using Hl7.Fhir.Rest;
+using hl7_test.Models;
+using System.Net.Http;
+namespace ConsoleApp1
+{
+    public partial class Form1 : Form
+    {
+        protected FhirClient _client;
+        Dictionary<string, Patient> Patients_dic = new Dictionary<string, Patient>();//This dic mimics the database, once Patient added to server we save refrence to its logical id
+        public Form1()
+        {
+            InitializeComponent();
+            //Initialize Fhir client
+            FhirClientSettings _settings = new FhirClientSettings()
+            {
+                PreferredFormat = ResourceFormat.Json,
+            };
+            //we use simplifier for testing and educational purposes
+            _client = new FhirClient("https://fhir.simplifier.net/fhir20231616", _settings);
+            _client.RequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoiYWRoYW1kZXNvdWt5IiwiaHR0cDovL3NjaGVtYXMueG1sc29hcC5vcmcvd3MvMjAwNS8wNS9pZGVudGl0eS9jbGFpbXMvbmFtZWlkZW50aWZpZXIiOiI4Mjc4ODE5Yy1kNWMzLTQ2YzgtYWY1ZS1lYzIyNjczMjk1MWUiLCJqdGkiOiJlOWRiOTI1NS1hODA2LTQ1ZjctOGYxNC03ZjliOGRlOTRkNGMiLCJleHAiOjE3MTM1ODk5NDksImlzcyI6ImFwaS5zaW1wbGlmaWVyLm5ldCIsImF1ZCI6ImFwaS5zaW1wbGlmaWVyLm5ldCJ9.59xoyNWN1itaSA4lwwZUs0mxwrrvM_fis3ZyU1YbY6Y.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoiemVpbmFzYWxhaCIsImh0dHA6Ly9zY2hlbWFzLnhtbHNvYXAub3JnL3dzLzIwMDUvMDUvaWRlbnRpdHkvY2xhaW1zL25hbWVpZGVudGlmaWVyIjoiNDcwYmU5ZDYtZDQ4ZC00YzQ3LWFjZjMtMTk5MjNjMDczMGY5IiwianRpIjoiNmE5ZjllODQtMmFhOC00OWQxLWE0NzItODBjYmRhNjAyNDhmIiwiZXhwIjoxNzEzNTA3MzgxLCJpc3MiOiJhcGkuc2ltcGxpZmllci5uZXQiLCJhdWQiOiJhcGkuc2ltcGxpZmllci5uZXQifQ.Nw1cPz5oS5aMtx5kJU5xuJWYfiOGVhLysYkK-aQv5j8");
+
+        }
+        private void Find_All_Patients_Btn_Click(object sender, EventArgs e)
+        {
+
+            var result = Find_Patient_By_Name(_client, "");
+            //bind list to data grid view
+            var _bind = result.resultList.Select(a => new { id = a.Id, Full_Name = a.FullName, Email = a.Email, BirthDate = a.Birthdate }).ToList();
+
+            dataGridView1.DataSource = _bind;
+
+
+            //dataGridView1.Columns["id"].Visible = false;
+
+        }
+        private void Find_Patient_Btn_Click(object sender, EventArgs e)
+        {
+            var result = Find_Patient_By_Name(_client, txt_Search_By_Name.Text);
+            //bind list to data grid view
+            var _bind = result.resultList.Select(a => new { id = a.Id, Full_Name = a.FullName, Email = a.Email, BirthDate = a.Birthdate }).ToList();
+            dataGridView1.DataSource = _bind;
+        }
+        private void Add_Patient_Btn_Click(object sender, EventArgs e)
+        {
+            //bind information to patientDataModel
+            PatientDataModel patientDataModel = new PatientDataModel();
+            patientDataModel.FirstName = txt_fname.Text;
+            patientDataModel.LastName = txt_LastName.Text;
+            patientDataModel.Email = txt_Email.Text;
+            patientDataModel.FullName = patientDataModel.FirstName + "" + patientDataModel.LastName;
+
+
+            //check Is patient already inserted or not based on full name
+            var result = Find_Patient_By_Name(_client, patientDataModel.FullName);
+
+            //check number of resources
+            if (result.resultList.Count > 0)
+            {
+                MessageBox.Show("already Added");
+            }
+            else
+            {
+                //fill in HL7 patient Model
+                Patient patient = new Patient()
+                {
+
+                    Active = true,
+                    Name = new List<HumanName>()
+                {
+                    new HumanName()
+                    {
+                        Family = patientDataModel.LastName,
+                        Text = patientDataModel.FullName,
+                        Given = new List<string>()
+                        {
+                            patientDataModel.FirstName
+                        },
+                         Use = HumanName.NameUse.Official
+                    }
+                },
+                    Identifier = new List<Identifier>()
+                    {
+                        new Identifier()
+                        {
+                            Value = "1032702",
+                            Use = Identifier.IdentifierUse.Usual,
+                            System="urn:oid:0.1.2.3.4.5.6.7"
+
+                        }
+                    },
+                    BirthDate = (new FhirDateTime(patientDataModel.Birthdate.Year, patientDataModel.Birthdate.Month, patientDataModel.Birthdate.Day)).ToString(),
+                    Gender = patientDataModel.Gender ? AdministrativeGender.Male : AdministrativeGender.Female,
+                    Telecom = new List<ContactPoint>()
+                {
+                    new ContactPoint()
+                    {
+                        System = ContactPoint.ContactPointSystem.Phone,
+                        Value = patientDataModel.phoneNumber,
+                        Use = ContactPoint.ContactPointUse.Mobile
+                    },
+                    new ContactPoint()
+                    {
+                        System = ContactPoint.ContactPointSystem.Phone,
+                        Value = patientDataModel.EmergencyNumber,
+                        Use = ContactPoint.ContactPointUse.Home
+                    },
+                     new ContactPoint()
+                    {
+                        System = ContactPoint.ContactPointSystem.Email,
+                        Value = patientDataModel.Email,
+                        Use = ContactPoint.ContactPointUse.Home
+                    }
+                }
+                };
+
+                var re = _client.Create<Patient>(patient);
+
+                if (re.Id != null)
+                {
+                    MessageBox.Show("Patient added");
+                    txt_fname.Text = "";
+                    txt_LastName.Text = "";
+                    txt_Email.Text = "";
+                    Patients_dic.Add(re.Id, patient);
+                }
+
+            }
+        }
+        private void Update_Patient_Btn_Click(object sender, EventArgs e)
+        {
+            var patient = Find_Patient_By_Name(_client, txt_Search_By_Name.Text);
+            if (patient != null)
+            {
+                var oldPatient = _client.Read<Patient>("Patient/" + patient.resultList.FirstOrDefault().Id);
+                //Generate dummy Date
+                DateTime dateTime = DateTime.Now;
+                oldPatient.BirthDate = (new FhirDateTime(dateTime.Year, dateTime.Month, dateTime.Day)).ToString();
+
+                //Update Patient
+                var re = _client.Update<Patient>(oldPatient);
+            }
+            else
+            {
+                MessageBox.Show("Patient ID not found in the Dictionary");
+            }
+
+        }
+        private void Delete_Patient_Btn_Click(object sender, EventArgs e)
+        {
+
+            var patient = Find_Patient_By_Name(_client, txt_Search_By_Name.Text);
+
+            //string patientKey_LogicalID = Patients_dic.Where(x => x.Value.Name.FirstOrDefault().Text == txt_Search_By_Name.Text).FirstOrDefault().Key;
+            //if (patientKey_LogicalID.Length != 0)
+            if (patient != null)
+            {
+                //Delete Patient
+                //_client.Delete("Patient/" + patientKey_LogicalID);
+                _client.Delete("Patient/" + patient.resultList.FirstOrDefault().Id);
+                MessageBox.Show("Patient Deleted");
+            }
+            else
+            {
+                MessageBox.Show("Patient ID not found in the Dictionary");
+            }
+
+        }
+        public static PatientDataModelList Find_Patient_By_Name(FhirClient _client, string full_name)
+        {
+            //initialize search paramtreres
+            SearchParams searchParams = new SearchParams()
+            {
+                Content = full_name,
+            };
+            //request sent
+            var result = _client.Search<Patient>(searchParams);
+
+            PatientDataModelList patients = new PatientDataModelList()
+            {
+                resultList = new List<PatientDataModel>(),
+                searchKey = full_name
+            };
+            //check resopnse
+            //convert each HL7 Patient Resource to PtientModel
+            result.Entry.ForEach(p =>
+            {
+                var _p = p.Resource as Patient;
+                PatientDataModel patient = new PatientDataModel()
+                {
+                    //fill in properties //check data availability to avoid exceptions!s
+                    Id = _p.Id,
+                    FullName = _p.Name.Any() ? string.IsNullOrEmpty(_p.Name.LastOrDefault().Text) ? _p.Name.LastOrDefault().Family : _p.Name.LastOrDefault().Text : "",
+                    IdentityNumber = _p.Identifier.Any() ? _p.Identifier.LastOrDefault().Value : "",
+                    Gender = _p.Gender == AdministrativeGender.Male,
+                    Email = _p.Telecom.Any() ?
+                                _p.Telecom.Where(x => x.System == ContactPoint.ContactPointSystem.Email).Any() ?
+                                _p.Telecom.Where(x => x.System == ContactPoint.ContactPointSystem.Email).FirstOrDefault().Value : ""
+                                : "",
+                    Birthdate = Convert.ToDateTime(_p.BirthDate)
+                };
+                //add PtientModel object to result list
+                patients.resultList.Add(patient);
+            });
+
+            return patients;
+
+        }
+
+    }
+}
